cecb07bdcfb03edd0fb6f68248144ba96b56b3e8
Alexei Ledenev <alexei.led@gmail.com>
2026-02-13 15:53:39 +0200

    ccbot 0.2.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ccbot.rb
+++ Formula/ccbot.rb
@@ -3,8 +3,8 @@ class Ccbot < Formula
 
   desc "Control Claude Code sessions remotely via Telegram"
   homepage "https://github.com/alexei-led/ccbot"
-  url "https://github.com/alexei-led/ccbot/archive/refs/tags/v0.2.5.tar.gz"
-  sha256 "4ab2f9c32fe56546f8857d321b87df06c7b399b2581a24878d84b9c0fe251ff6"
+  url "https://github.com/alexei-led/ccbot/archive/refs/tags/v0.2.6.tar.gz"
+  sha256 "93c3c4dbe4e2a25cb3ef396bff2ea2111132dc98a64f8c5d5625b6bddd58ea6e"
   license "MIT"
 
   depends_on "python@3.14"
